580030686b40e2e796c98b8c1c407df83ff988c1
Hemang Kandwal <2285702k@student.gla.ac.uk>
2018-05-24 20:25:50 +0100

    Made a simple working t-rex game

--- main.py
+++ main.py
@@ -1,8 +1,78 @@
 import sys,os
 import curses
+import time
+import threading
+from enum import Enum
+
+class Time(Enum):
+    IDEAL = 0.02
+    JUMP = 0.012
+
+k = 0
+global_stdscr = None
+
+JUMP_HEIGHT = 14
+IDLE_FRAMES = [(28,25,7),(29,24,2),(29,27,6),(30,24,9),(31,24,3),(32,24,7),
+        (33,23,3),(34,22,5),(35,20,8),(36,19,13),(37,18,10),(37,31,1),(38,18,9),
+        (39,12,15),(38,12,3),(37,12,2),(36,12,1),
+        (40,13,13),(41,14,11),
+        (42,16,3),(42,21,3),(43,17,2),(43,22,2),
+        (44,18,2),(44,23,2)]
+WALK_ONE_FRAMES = [(28,25,7),(29,24,2),(29,27,6),(30,24,9),(31,24,3),(32,24,7),
+        (33,23,3),(34,22,5),(35,20,8),(36,19,13),(37,18,10),(37,31,1),(38,18,9),
+        (39,12,15),(38,12,3),(37,12,2),(36,12,1),
+        (40,13,13),(41,14,11),
+        (42,16,3),(42,21,3),(43,17,2),(43,22,3),
+        (44,18,2)]
+WALK_TWO_FRAMES = [(28,25,7),(29,24,2),(29,27,6),(30,24,9),(31,24,3),(32,24,7),
+        (33,23,3),(34,22,5),(35,20,8),(36,19,13),(37,18,10),(37,31,1),(38,18,9),
+        (39,12,15),(38,12,3),(37,12,2),(36,12,1),
+        (40,13,13),(41,14,11),
+        (42,16,3),(42,21,3),(43,17,3),(43,22,2),
+        (44, 23, 2)]
+JUMP_SENSITIVITY = 0.002
+ANIMATE_SENSITIVITY = 5
+
+idle = True
+SLEEP_TIME = Time.IDEAL.value
+
+def get_input():
+    global k,global_stdscr,idle
+    while k!=ord('q'):
+        k = global_stdscr.getch()
+        if not idle and k==curses.KEY_UP:
+            k=0
+
+
+def setup_foreground(stdscr,height,width):
+    stdscr.hline(height-11, 0, "_", width)
+
+
+def create_obstacles(obstacle_index,stdscr,height):
+    #stem
+    stdscr.chgat(height - 11, obstacle_index, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 12, obstacle_index, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 13, obstacle_index, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 14, obstacle_index, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 15, obstacle_index, 1, curses.A_REVERSE)
+
+    #right branch
+    stdscr.chgat(height - 13, obstacle_index+1, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 13, obstacle_index+2, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 14, obstacle_index+2, 1, curses.A_REVERSE)
+
+    #left branch
+    stdscr.chgat(height - 12, obstacle_index-1, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 12, obstacle_index-2, 1, curses.A_REVERSE)
+    stdscr.chgat(height - 13, obstacle_index-2, 1, curses.A_REVERSE)
+
+
+
+
 
 def draw_menu(stdscr):
-    k = 0
+    global k,global_stdscr,idle,SLEEP_TIME
+    global_stdscr = stdscr
     cursor_x = 0
     cursor_y = 0
 
@@ -10,109 +80,88 @@ def draw_menu(stdscr):
     stdscr.clear()
     stdscr.refresh()
 
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+
+    height, width = global_stdscr.getmaxyx()
+    obstacle_index = width - 5
+    index = 0
+    jump = going_down = going_up = on_ground = False
+
+    type_idle = 3
+    animate_count = 0
 
     # Loop where k is the last character pressed
     while (k != ord('q')):
-
-        # Initialization
         stdscr.clear()
+        # Initialization
         height, width = stdscr.getmaxyx()
 
-        if k == curses.KEY_DOWN:
-            cursor_y = cursor_y + 1
-        elif k == curses.KEY_UP:
-            cursor_y = cursor_y - 1
-        elif k == curses.KEY_RIGHT:
-            cursor_x = cursor_x + 1
-        elif k == curses.KEY_LEFT:
-            cursor_x = cursor_x - 1
+        if k == curses.KEY_UP:
+            if idle:
+                jump = going_up = True
+                idle = False
+                k=0
 
-        cursor_x = max(0, cursor_x)
-        cursor_x = min(width-1, cursor_x)
+        setup_foreground(stdscr,height,width)
+        create_obstacles(obstacle_index, stdscr, height)
 
-        cursor_y = max(0, cursor_y)
-        cursor_y = min(height-1, cursor_y)
+        if obstacle_index >= 5:
+            obstacle_index -= 1
+        else:
+            obstacle_index = width-5
 
-        # stdscr.chgat(height-1,0,width-1,curses.A_REVERSE)
-        # stdscr.chgat(height-2,1,width-3,curses.A_REVERSE)
-        # stdscr.chgat(height-3,2,width-5,curses.A_REVERSE)
+        if idle:
+            if type_idle == 1:
+                for part in IDLE_FRAMES:
+                    stdscr.chgat(part[0], part[1], part[2], curses.A_REVERSE)
+                if animate_count>ANIMATE_SENSITIVITY:
+                    type_idle=2
+                    animate_count=0
+            elif type_idle == 2:
+                for part in WALK_ONE_FRAMES:
+                    stdscr.chgat(part[0], part[1], part[2], curses.A_REVERSE)
+                if animate_count > ANIMATE_SENSITIVITY:
+                    type_idle = 3
+                    animate_count = 0
+            elif type_idle == 3:
+                for part in WALK_TWO_FRAMES:
+                    stdscr.chgat(part[0], part[1], part[2], curses.A_REVERSE)
+                if animate_count>ANIMATE_SENSITIVITY:
+                    type_idle=1
+                    animate_count=0
+            animate_count += 1
 
-
-        #T Rex design
-
-
-        stdscr.chgat(26,25,8 ,curses.A_REVERSE)
-        stdscr.chgat(27,24,2,curses.A_REVERSE)
-        stdscr.chgat(27,27,9,curses.A_REVERSE)
-        stdscr.chgat(28,24,11,curses.A_REVERSE)
-        stdscr.chgat(29,24,11,curses.A_REVERSE)
-        stdscr.chgat(30,24,11,curses.A_REVERSE)
-        stdscr.chgat(31,24,3,curses.A_REVERSE)
-        stdscr.chgat(32,24,7,curses.A_REVERSE)
-
-        #body
-        stdscr.chgat(33,23,3,curses.A_REVERSE)
-        stdscr.chgat(34,22,5,curses.A_REVERSE)
-        stdscr.chgat(35,20,8,curses.A_REVERSE)
-        stdscr.chgat(36,19,13,curses.A_REVERSE)
-        stdscr.chgat(37,18,10,curses.A_REVERSE)
-        stdscr.chgat(37,31,1,curses.A_REVERSE)
-        stdscr.chgat(38,18,9,curses.A_REVERSE)
-
-        # tail
-        stdscr.chgat(39,12,15,curses.A_REVERSE)
-        stdscr.chgat(38,12,3,curses.A_REVERSE)
-        stdscr.chgat(37,12,2,curses.A_REVERSE)
-        stdscr.chgat(36,12,1,curses.A_REVERSE)
-
-        #down body
-        stdscr.chgat(40,13,13,curses.A_REVERSE)
-        stdscr.chgat(41,14,11,curses.A_REVERSE)
-
-        #legs
-        stdscr.chgat(42,16,3,curses.A_REVERSE)
-        stdscr.chgat(42,21,3,curses.A_REVERSE)
-        stdscr.chgat(43,17,2,curses.A_REVERSE)
-        stdscr.chgat(43,22,2,curses.A_REVERSE)
-
-        #ankles
-        stdscr.chgat(44,18,2,curses.A_REVERSE)
-        stdscr.chgat(44,23,2,curses.A_REVERSE)
-
-
-
-        # stdscr.hline(height-10,0,"_",width//2-4)
-        # stdscr.chgat(height-10,width//2-4,1,curses.A_REVERSE)
-        # stdscr.chgat(height-11,width//2-3,1,curses.A_REVERSE)
-        # stdscr.chgat(height-12,width//2-2,1,curses.A_REVERSE)
-        # stdscr.chgat(height-12,width//2-1,1,curses.A_REVERSE)
-        # stdscr.chgat(height-12,width//2,1,curses.A_REVERSE)
-        # stdscr.chgat(height-12,width//2+1,1,curses.A_REVERSE)
-        # stdscr.chgat(height-12,width//2+2,1,curses.A_REVERSE)
-        # stdscr.chgat(height-11,width//2+3,1,curses.A_REVERSE)
-        # stdscr.chgat(height-10,width//2+4,1,curses.A_REVERSE)
-        # stdscr.hline(height-10,width//2+5,"_",width//2-4)
-
-
-
+        elif jump:
+            if going_up:
+                if index<=JUMP_HEIGHT:
+                    for part in IDLE_FRAMES:
+                        stdscr.chgat(part[0]-index,part[1],part[2],curses.A_REVERSE)
+                    index+=1
+                else:
+                    going_down = True
+                    going_up = False
+            elif going_down:
+                if index>=0:
+                    for part in IDLE_FRAMES:
+                        stdscr.chgat(part[0]-index, part[1], part[2], curses.A_REVERSE)
+                    index-=1
+                else:
+                    going_down = False
+                    jump = False
+                    idle = True
 
         stdscr.move(0,0)
 
 
-
-        # Refresh the screen
-        stdscr.refresh()
-
         # Wait for next input
-        k = stdscr.getch()
+        time.sleep(SLEEP_TIME)
+        stdscr.refresh()
 
 def main():
     curses.wrapper(draw_menu)
 
 if __name__ == "__main__":
-    main()
+    t1 = threading.Thread(target=main, args=[])
+    t2 = threading.Thread(target=get_input, args=[])
+    t1.start()
+    t2.start()
